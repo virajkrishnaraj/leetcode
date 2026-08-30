71a7125b5767f440c424d72824159061df062649
Viraj Krishnaraj <virajkrishnaraj25@gmail.com>
2026-08-30 10:18:21 +0530

    Time: 5 ms (4.21%), Space: 12.5 MB (16.07%) - LeetHub

---
+++ 2769-find-the-maximum-achievable-number/2769-find-the-maximum-achievable-number.py
@@ -0,0 +1,13 @@
+class Solution(object):
+    def theMaximumAchievableX(self, num, t):
+        x = num + 2*t
+        
+        return x
+
+# x = x - t
+# num = num + t
+
+# x - t = num + t
+
+# x = num + 2t
+        
