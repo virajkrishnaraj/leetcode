d0b4cd82b6b90633441bb0d792a0fb0077c12b43
Viraj Krishnaraj <virajkrishnaraj25@gmail.com>
2026-09-07 23:00:20 +0530

    Time: 0 ms (100%), Space: 12.4 MB (51.58%) - LeetHub

---
+++ 1550-three-consecutive-odds/1550-three-consecutive-odds.py
@@ -0,0 +1,8 @@
+class Solution(object):
+    def threeConsecutiveOdds(self, arr):
+        for i in range(len(arr)-2):
+            if arr[i]%2!=0 and arr[i+1]%2!=0 and arr[i+2]%2!=0:
+                return True
+        return False
+
+        
