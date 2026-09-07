462783536f3b36050a75ad9186bc704d877de05b
Viraj Krishnaraj <virajkrishnaraj25@gmail.com>
2026-09-07 23:32:44 +0530

    Time: 30 ms (61.49%), Space: 12.7 MB (18.17%) - LeetHub

--- 0657-robot-return-to-origin/0657-robot-return-to-origin.py
+++ 0657-robot-return-to-origin/0657-robot-return-to-origin.py
@@ -1,7 +1,11 @@
 class Solution(object):
     def judgeCircle(self, moves):
-        if moves.count("L") == moves.count("R") and moves.count("U") == moves.count("D"):
+        counts = {'L': 0, 'R': 0, 'U': 0, 'D': 0}
+        for ch in moves:
+            counts[ch] += 1
+        
+        if counts["L"] == counts["R"] and counts["U"] == counts["D"]:
             return True
         else:
-            return False
+            return False 
         
